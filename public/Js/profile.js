3cd439eca515b68caa617f2274a8a65f4cc058c5
Mahyar <112708935+asloberg@users.noreply.github.com>
2025-02-24 13:58:46 +0330

    Add files via upload

---
+++ public/Js/profile.js
@@ -0,0 +1,6 @@
+document.getElementById("upload-btn").onclick = function () {
+    window.location.href = 'http://localhost:3000/upload';
+}
+document.getElementById("settings-btn").onclick = function () {
+    window.location.href = 'http://localhost:3000/profile/setting'
+}
