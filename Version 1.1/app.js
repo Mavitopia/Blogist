c73f5aa7f20307d52ef09a4a08b6f796d9c65bff
Mahyar <mpgmahyar@gmail.com>
2025-06-06 18:48:31 +0330

    finished designing the main pages
    the main pages are almost done and it just need some improvments

--- Version 1.1/app.js
+++ Version 1.1/app.js
@@ -1,26 +1,32 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 
 const port = 3000;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.get('/', (req,res) => {
-    res.render('index.ejs');
-})
+app.get("/quest", (req, res) => {
+  res.render("quest.ejs");
+});
 
-app.get('/quest', (req,res) => {
-    res.render('quest.ejs');
-})
+app.get("/guild", (req, res) => {
+  res.render("guild.ejs");
+});
 
-app.get('/guild', (req,res) => {
-    res.render('guild.ejs');
-})
+app.get("/blog", (req, res) => {
+  res.render("blog.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    }
-)
+  console.log(`Server is running on port ${port}`);
+});
